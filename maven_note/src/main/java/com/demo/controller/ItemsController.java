3ba21d85b0b3f1bd4497c13b0ed233603406473e
chenggang <chenggang@hua-yong.com>
2020-03-24 16:05:00 +0800

    controller添加

---
+++ maven_note/src/main/java/com/demo/controller/ItemsController.java
@@ -0,0 +1,22 @@
+package com.demo.controller;
+
+import com.demo.dao.Items;
+import com.demo.service.ItemsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/items")
+public class ItemsController {
+    @Autowired
+    private ItemsService itemsService;
+
+    @RequestMapping(value = "/item")
+    @ResponseBody
+    public Items findDetail(){
+        Items items = itemsService.findItemById(1);
+        return items;
+    }
+}
