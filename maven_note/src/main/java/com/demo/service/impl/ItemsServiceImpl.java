edee02668b2ded8df94b9043d727299e0e1139a0
chenggang <chenggang@hua-yong.com>
2020-03-24 10:17:33 +0800

    调整

--- maven_note/src/main/java/com/demo/service/impl/ItemsServiceImpl.java
+++ maven_note/src/main/java/com/demo/service/impl/ItemsServiceImpl.java
@@ -2,11 +2,12 @@ package com.demo.service.impl;
 
 import com.demo.dao.Items;
 import com.demo.mapper.ItemsMapper;
+import com.demo.service.ItemsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ItemsServiceImpl implements ItemsMapper {
+public class ItemsServiceImpl implements ItemsService {
     @Autowired
     private ItemsMapper itemsMapper;
 
