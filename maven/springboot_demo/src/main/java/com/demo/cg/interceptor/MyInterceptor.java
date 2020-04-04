5d342844550249ef18f0f73d9b90e779ac44da15
chenggang <chenggang@hua-yong.com>
2020-04-05 01:20:49 +0800

    注释

--- maven/springboot_demo/src/main/java/com/demo/cg/interceptor/MyInterceptor.java
+++ maven/springboot_demo/src/main/java/com/demo/cg/interceptor/MyInterceptor.java
@@ -1,9 +1,6 @@
 package com.demo.cg.interceptor;
 
-import lombok.extern.log4j.Log4j;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -15,17 +12,17 @@ public class MyInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        log.debug("preHandle methods");
+        // log.debug("preHandle methods");
         return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) {
-        log.debug("postHandle methods");
+        // log.debug("postHandle methods");
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
-        log.debug("afterCompletion methods");
+        // log.debug("afterCompletion methods");
     }
 }
