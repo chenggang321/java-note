aaf42cd194cfc57c881f00a7a25e158188aa28c0
chenggang <chenggang@hua-yong.com>
2020-04-05 23:42:45 +0800

    动态获取ip和port

--- maven/cloud_demo/customer_demo/src/main/java/com/demo/cg/web/CustomerController.java
+++ maven/cloud_demo/customer_demo/src/main/java/com/demo/cg/web/CustomerController.java
@@ -2,11 +2,15 @@ package com.demo.cg.web;
 
 import com.demo.cg.pojo.Items;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("customer")
@@ -14,10 +18,16 @@ public class CustomerController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("{id}")
     public Items queryById(@PathVariable("id") Long id){
-        String url = "http://localhost:9999/items/" + id;
-        Items items = restTemplate.getForObject(url,Items.class);
-        return items;
+        // 获取服务实例
+        List<ServiceInstance> instances = discoveryClient.getInstances("items-service");
+        // 从实例中获取ip和端口
+        ServiceInstance instance = instances.get(0);
+        String url = "http://"+instance.getHost()+":"+instance.getPort()+"/items/" + id;
+        return restTemplate.getForObject(url,Items.class);
     }
 }
--- maven/cloud_demo/item_service/src/main/java/com/demo/cg/ItemsApplication.java
+++ maven/cloud_demo/item_service/src/main/java/com/demo/cg/ItemsApplication.java
@@ -2,8 +2,10 @@ package com.demo.cg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan("com.demo.cg.mapper")
 public class ItemsApplication {
